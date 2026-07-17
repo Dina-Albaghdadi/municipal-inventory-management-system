fd69e4e9e42fdd8e00964e4ba7e59830e280379f
Dina-Albaghdadi <dinalbaghdadi@gmail.com>
2026-07-17 14:15:48 +0300

    Remove file

--- resources/views/categories/show.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>تفاصيل الفئة: {{ $category->name }}</h1>
-<p><strong>الوصف:</strong> {{ $category->description }}</p>
-<p><strong>تاريخ الإنشاء:</strong> {{ $category->created_at }}</p>
-<a href="{{ route('categories.index') }}">العودة للقائمة</a>
